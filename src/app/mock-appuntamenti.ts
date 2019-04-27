a089bdb32931b895716203f09ad2cb87d283697d
Fabio Malizia <fmalizia@gmail.com>
2019-04-27 14:41:08 +0200

    primo commit

---
+++ src/app/mock-appuntamenti.ts
@@ -0,0 +1,7 @@
+import { Appuntamento } from './appuntamento';
+
+export const Appuntamenti: Appuntamento[] = [
+  { nome: 'Luca',  email: 'luca.giurato@gmlai.it', data: '27/06/2019', ora: '10:00'},
+  { nome: 'Maurizio', email: 'mauri.costanzo@gmail.com', data: '07/08/2019', ora: '09:30'},
+  { nome: 'Paolo',  email: 'brosio.paolo@gmail.com', data: '10/08/2019', ora: '11.50'}
+];
